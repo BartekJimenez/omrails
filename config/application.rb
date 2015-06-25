72d0e189ec229db01e03a7dc64117605047ae214
Zealex <Pwentz9000@yahoo.com>
2015-06-25 19:08:04 -0400

    Add User Model with Devise

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module Omrails
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    #required for devise on heroku
+    config.assets.initialize_on_precompile = false
   end
 end
